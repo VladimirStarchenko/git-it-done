08ec9a3b8608a3af392595d1a8e9640cb17dd1c0
Vladimir Starchenko <vladimistar@gmail.com>
2021-11-17 11:58:02 -0500

    used query parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
   // format the github api url
@@ -19,10 +20,25 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      // id not succesful, redirect to homepage
+      document.location.replace("./index.html");
     }
   });
+};
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on page
+    repoNameEl.textContent = repoName;
+
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to homepage
+    document.location.replace("./index.html");
+  }
 };
 
 var displayIssues = function (issues) {
@@ -78,4 +94,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
